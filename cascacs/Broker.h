d6d547a57bd5d5364472edbb7ceae75a9f73aab2
Lucho <luiscarlossomoza@gmail.com>
2023-05-21 22:28:11 -0400

    Paso de parametros entre hilos

--- cascacs/Broker.h
+++ cascacs/Broker.h
@@ -3,15 +3,27 @@
 
 #include <zmq.hpp>
 #include <zmq_addon.hpp>
+#include <pthread.h>
+#include "shared.h"
 
-int Broker()
+void* reader_thread(void*){
+    //Debo cerrar la variable para mantener la consistencia
+    pthread_mutex_lock(&pthread_mutex);
+    std::cout << "Broker lee: " << shared_variable << std::endl;
+    pthread_mutex_unlock(&pthread_mutex);
+    //Termino con la variable y la libero
+    return NULL;
+
+}
+
+static void* Broker(void* nullarg)
 {
-    // initialize the zmq context with a single IO thread
+    std::cout << "Broker iniciado" << std::endl;
     zmq::context_t context{1};
-
     // construct a REQ (request) socket and connect to interface
     zmq::socket_t subscriber{context, zmq::socket_type::sub};
     zmq::socket_t publisher{context, zmq::socket_type::pub};
+
     subscriber.connect("tcp://localhost:5555");
     publisher.bind("tcp://*:5556");
 
@@ -19,6 +31,8 @@ int Broker()
     subscriber.setsockopt(ZMQ_SUBSCRIBE,"",0);
 
     while(true){
+        //Lee variable compartida
+        reader_thread(NULL);
         // receive a message
         zmq::message_t topic;
         zmq::message_t content;
@@ -27,18 +41,21 @@ int Broker()
         if (received) {
             std::string topic_str(static_cast<char*>(topic.data()), topic.size());
             std::string content_str(static_cast<char*>(content.data()), content.size());
+
             if(topic_str == "status"){
                 //Buscar todos los subscriptores asociados
                 //al topico y mandar el contenido
                 publisher.send(zmq::str_buffer("status"), zmq::send_flags::none);
                 publisher.send(zmq::str_buffer("Mensaje desde broker"));
             }
+
             std::cout << "Received message on topic \"" << topic_str << "\": " << content_str << std::endl;
         } else {
             // handle receive errors
             std::cerr << "Error receiving message: " << zmq_strerror(errno) << std::endl;
         }
     }
-
-    return 0;
+    subscriber.close();
+    publisher.close();
+    return NULL;
 }
--- cascacs/main.cpp
+++ cascacs/main.cpp
@@ -1,13 +1,38 @@
 #include "mainwindow.h"
 #include "zmq.h"
+#include "zmq.hpp"
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
 #include <iostream>
 #include "Broker.h"
+#include "Publisher.h"
+#include <thread>
+#include <pthread.h>
+#include <mutex> //Libreria para crear objetos compartidos entre hilos
+#include <condition_variable> //Indica cambio entre hilos para compartir variable
+#include "shared.h"
 
 using namespace std;
-int main()
+
+int shared_variable = 0;
+pthread_mutex_t pthread_mutex;
+
+int main(void)
 {
-    Broker();
+
+    pthread_mutex_init(&pthread_mutex, NULL); //Espacio donde se comparten datos del mutex
+
+    std::vector<std::thread> threads; //Vector que almacena todos los procesos clientes (publicadores y suscriptores)
+    pthread_t broker_thread;
+    pthread_t client_thread;
+    pthread_create(&broker_thread,NULL,&Broker,NULL);
+    pthread_create(&client_thread,NULL,&Publisher,NULL);
+
+    pthread_join(broker_thread,NULL);
+    pthread_join(client_thread,NULL);
+
+    pthread_mutex_destroy(&pthread_mutex);
+
+
 }
