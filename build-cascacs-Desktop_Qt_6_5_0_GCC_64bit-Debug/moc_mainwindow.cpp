23f6b2aebfa908067a59e2861269f46ff61ee1cd
César Sotillo <cesarsotillo16@gmail.com>
2023-05-25 00:28:39 -0400

    Pub/Sub en Paho
    Fino señores

--- build-cascacs-Desktop_Qt_6_5_0_GCC_64bit-Debug/moc_mainwindow.cpp
+++ build-cascacs-Desktop_Qt_6_5_0_GCC_64bit-Debug/moc_mainwindow.cpp
@@ -38,20 +38,52 @@ namespace {
 #ifdef QT_MOC_HAS_STRINGDATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
 static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
-    "MainWindow"
+    "MainWindow",
+    "on_crear_sub_btn_clicked",
+    "",
+    "on_listar_subcriptores_btn_clicked",
+    "std::vector<int>&",
+    "int_list",
+    "on_servicios_subscritos_btn_clicked",
+    "on_subscribir_btn_clicked",
+    "on_listar_publicaciones_btn_clicked"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[2];
+    uint offsetsAndSizes[18];
     char stringdata0[11];
+    char stringdata1[25];
+    char stringdata2[1];
+    char stringdata3[35];
+    char stringdata4[18];
+    char stringdata5[9];
+    char stringdata6[36];
+    char stringdata7[26];
+    char stringdata8[36];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
 Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
     {
-        QT_MOC_LITERAL(0, 10)   // "MainWindow"
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 24),  // "on_crear_sub_btn_clicked"
+        QT_MOC_LITERAL(36, 0),  // ""
+        QT_MOC_LITERAL(37, 34),  // "on_listar_subcriptores_btn_cl..."
+        QT_MOC_LITERAL(72, 17),  // "std::vector<int>&"
+        QT_MOC_LITERAL(90, 8),  // "int_list"
+        QT_MOC_LITERAL(99, 35),  // "on_servicios_subscritos_btn_c..."
+        QT_MOC_LITERAL(135, 25),  // "on_subscribir_btn_clicked"
+        QT_MOC_LITERAL(161, 35)   // "on_listar_publicaciones_btn_c..."
     },
-    "MainWindow"
+    "MainWindow",
+    "on_crear_sub_btn_clicked",
+    "",
+    "on_listar_subcriptores_btn_clicked",
+    "std::vector<int>&",
+    "int_list",
+    "on_servicios_subscritos_btn_clicked",
+    "on_subscribir_btn_clicked",
+    "on_listar_publicaciones_btn_clicked"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -63,12 +95,26 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   44,    2, 0x08,    1 /* Private */,
+       3,    1,   45,    2, 0x08,    2 /* Private */,
+       6,    0,   48,    2, 0x08,    4 /* Private */,
+       7,    0,   49,    2, 0x08,    5 /* Private */,
+       8,    0,   50,    2, 0x08,    6 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -81,17 +127,36 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
     nullptr,
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
         // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_crear_sub_btn_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_listar_subcriptores_btn_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<std::vector<int> &, std::false_type>,
+        // method 'on_servicios_subscritos_btn_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_subscribir_btn_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_listar_publicaciones_btn_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
 } };
 
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
-    (void)_a;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_crear_sub_btn_clicked(); break;
+        case 1: _t->on_listar_subcriptores_btn_clicked((*reinterpret_cast< std::add_pointer_t<std::vector<int>&>>(_a[1]))); break;
+        case 2: _t->on_servicios_subscritos_btn_clicked(); break;
+        case 3: _t->on_subscribir_btn_clicked(); break;
+        case 4: _t->on_listar_publicaciones_btn_clicked(); break;
+        default: ;
+        }
+    }
 }
 
 const QMetaObject *MainWindow::metaObject() const
@@ -110,6 +175,17 @@ void *MainWindow::qt_metacast(const char *_clname)
 int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 5;
+    }
     return _id;
 }
 QT_WARNING_POP
